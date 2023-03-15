11c69e70973e4c3c4c27714f5c0d4651f84aca07
artursuskevits@gmail.com <artursuskevits@gmail.com>
2023-03-15 12:48:22 -0700

    kodutoo

--- login.py
+++ login.py
@@ -1,21 +1,41 @@
 from module1 import *
 from tkinter import *
-logins=[]
-passwords=[]
+logins={"admin":"admin1"}
+passwords={"admin1":"admin"}
 aken=Tk()
 aken.geometry("600x600")
 aken.title("Menu")
 aken.iconbitmap("images.ico")
 
-def check_registreerimine(ent1,ent2):
+def end():
+    aken.destroy()
+
+def check_autoriseerimine(ent1,ent2,dopaken):
+    if ent1.get() != "" and ent2.get() != "":
+        login = str(ent1.get())
+        password = str(ent2.get())
+        if logins[login]==password and passwords[password] == login:
+            dopaken.destroy()
+            dopaken=Toplevel()
+            lbl4=Label(dopaken,text="You in",height=2,width=40,font="Arial 24")
+            lbl4.pack()
+        else:
+            ent1.configure(bg="red")
+            ent2.configure(bg="red")
+    else:
+        ent1.configure(bg="red")
+        ent2.configure(bg="red")
+
+def check_registreerimine(ent1,ent2,dopaken):
     if ent1.get() != "" and ent2.get() != "":
         login = str(ent1.get())
         print(login)
-        password=str(ent2.get())
-        logins.append(ent1.get())
-        passwords.append(ent2.get())
+        password = str(ent2.get())
+        logins.update({login:password})
+        passwords.update({password:login})
         print(logins)
         print(passwords)
+        dopaken.destroy()
     else:
         ent1.configure(bg="red")
         ent2.configure(bg="red")
@@ -29,7 +49,7 @@ def registreerimine():
     ent1=Entry(dopaken,fg="blue",bg="lightblue",width=15,font="Arial 20", justify=CENTER)
     lbl2=Label(dopaken,text="Create password")
     ent2=Entry(dopaken,fg="blue",bg="lightblue",width=15,font="Arial 20", justify=CENTER)
-    Check=Button(dopaken,text="go", font="Arial 24",relief=RAISED,command=lambda:check_registreerimine(ent1,ent2))
+    Check=Button(dopaken,text="go", font="Arial 24",relief=RAISED,command=lambda:check_registreerimine(ent1,ent2,dopaken))
     lbl1.pack()
     ent1.pack()
     lbl2.pack()
@@ -37,16 +57,154 @@ def registreerimine():
     Check.pack()
     dopaken.mainloop()
 
+def autoriseerimine():
+    dopaken=Toplevel()
+    dopaken.geometry("600x600")
+    dopaken.title("autoriseerimine")
+    lbl1=Label(dopaken,text="Insert login")
+    ent1=Entry(dopaken,fg="blue",bg="lightblue",width=15,font="Arial 20", justify=CENTER)
+    lbl2=Label(dopaken,text="Insert password")
+    ent2=Entry(dopaken,fg="blue",bg="lightblue",width=15,font="Arial 20", justify=CENTER)
+    Check=Button(dopaken,text="go", font="Arial 24",relief=RAISED,command=lambda:check_autoriseerimine(ent1,ent2,dopaken))
+    lbl1.pack()
+    ent1.pack()
+    lbl2.pack()
+    ent2.pack()
+    Check.pack()
+    dopaken.mainloop()
+
+def muuda():
+    dopaken=Toplevel()
+    dopaken.geometry("600x600")
+    dopaken.title("nime voi parooli muutmine")
+    lbl1=Label(dopaken,text="What you want change?",font="Arial 24")
+    Lobtn=Button(dopaken, text="Login", font="Arial 24",relief=RAISED,command=lambda:lchange(dopaken))
+    Pabtn=Button(dopaken, text="Password", font="Arial 24",relief=RAISED,command=lambda:pchange(dopaken))
+    lbl1.pack()
+    Lobtn.pack()
+    Pabtn.pack()
+
+def lchange(dopaken):
+    dopaken.destroy()
+    dopaken2=Toplevel()
+    dopaken2.geometry("600x600")
+    lbl1=Label(dopaken2,text="Write old Login",font="Arial 24")
+    ent1=Entry(dopaken2,fg="blue",bg="lightblue",width=15,font="Arial 20", justify=CENTER)
+    lbl3=Label(dopaken2,text="Write your Pass",font="Arial 24")
+    ent3=Entry(dopaken2,fg="blue",bg="lightblue",width=15,font="Arial 20", justify=CENTER)
+    lbl2=Label(dopaken2,text="Write new Login",font="Arial 24")
+    ent2=Entry(dopaken2,fg="blue",bg="lightblue",width=15,font="Arial 20", justify=CENTER)
+    chng=Button(dopaken2, text="Change", font="Arial 24",relief=RAISED,command=lambda:checkl(ent1,ent3,ent2,dopaken2))
+    lbl1.pack()
+    ent1.pack()
+    lbl3.pack()
+    ent3.pack()
+    lbl2.pack()
+    ent2.pack()
+    chng.pack()
+def checkl(ent1,ent3,ent2,dopaken2):
+    if ent1.get() != "" and ent2.get() != "":
+        yourpass=ent3.get()
+        oldlogin=ent1.get()
+        changelogin=ent2.get()
+        if oldlogin in logins and yourpass in passwords and passwords[yourpass]==oldlogin:
+            oldpass=logins[oldlogin]
+            passwords[oldpass] = changelogin
+            logins[changelogin]=logins.pop(oldlogin)
+            print(logins)
+            print(passwords)
+            dopaken2.destroy()
+        else:
+            ent1.configure(bg="red")
+            ent2.configure(bg="red")
+            ent3.configure(bg="red")
+    else:
+        ent1.configure(bg="red")
+        ent2.configure(bg="red")
+        ent3.configure(bg="red")
+
+
+def pchange(dopaken):
+    dopaken.destroy()
+    dopaken2=Toplevel()
+    dopaken2.geometry("600x600")
+    lbl3=Label(dopaken2,text="Write your login",font="Arial 24")
+    ent3=Entry(dopaken2,fg="blue",bg="lightblue",width=15,font="Arial 20", justify=CENTER)
+    lbl1=Label(dopaken2,text="Write old Password",font="Arial 24")
+    ent1=Entry(dopaken2,fg="blue",bg="lightblue",width=15,font="Arial 20", justify=CENTER)
+    lbl2=Label(dopaken2,text="Write new Password",font="Arial 24")
+    ent2=Entry(dopaken2,fg="blue",bg="lightblue",width=15,font="Arial 20", justify=CENTER)
+    chng=Button(dopaken2, text="Change", font="Arial 24",relief=RAISED,command=lambda:checkp(ent1,ent2,ent3,dopaken2))
+    lbl3.pack()
+    ent3.pack()
+    lbl1.pack()
+    ent1.pack()
+    lbl2.pack()
+    ent2.pack()
+    chng.pack()
+def checkp(ent1,ent2,ent3,dopaken2):
+    if ent1.get() != "" and ent2.get() != "" and ent3 != "":
+        yourlogin=ent3.get()
+        oldpassword=ent1.get()
+        changepassword=ent2.get()
+        if oldpassword in passwords and yourlogin in logins and logins[yourlogin] == oldpassword :
+            oldlogin=passwords[oldpassword]
+            logins[oldlogin] = changepassword
+            passwords[changepassword]=passwords.pop(oldpassword)
+            print(logins)
+            print(passwords)
+            dopaken2.destroy()
+        else:
+            ent1.configure(bg="red")
+            ent2.configure(bg="red")
+            ent3.configure(bg="red")
+    else:
+        ent1.configure(bg="red")
+        ent2.configure(bg="red")
+        ent3.configure(bg="red")
+
+def help():
+    dopaken=Toplevel()
+    dopaken.geometry("600x600")
+    dopaken.title("unustanud parooli taastamine")
+    lbl1=Label(dopaken,text="Write your login",font="Arial 24")
+    ent3=Entry(dopaken,fg="blue",bg="lightblue",width=15,font="Arial 20", justify=CENTER)
+    chck=Button(dopaken, text="show password", font="Arial 24",relief=RAISED,command=lambda:showpassword(ent3,dopaken))
+    lbl1.pack()
+    ent3.pack()
+    chck.pack()
+
+def showpassword(ent3,dopaken):
+    yourlogin=ent3.get()
+    dopaken.destroy()
+    dopaken2=Toplevel()
+    dopaken2.geometry("600x600")
+    lbl2=Label(dopaken2,text="Your password:",font="Arial 24")
+    lbl3=Label(dopaken2,font="Arial 30")
+    if yourlogin in logins:
+        yourpassword=logins[yourlogin]
+        lbl3.configure(text=yourpassword)
+        lbl2.pack()
+        lbl3.pack()
+    else:
+        lbl3.configure(bg="red")
+        lbl3.configure(text="Wrong Login")
+        lbl2.pack()
+        lbl3.pack()
+
+        
+
 
 
 btn1=Button(aken, text="registreerimine", font="Arial 24",relief=RAISED,command=registreerimine)
-btn2=Button(aken, text="autoriseerimine", font="Arial 24",relief=RAISED)
-btn3=Button(aken, text="nime voi parooli muutmine", font="Arial 24",relief=RAISED)
-btn4=Button(aken, text="unustanud parooli taastamine", font="Arial 24",relief=RAISED)
-btn5=Button(aken, text="lopetamine", font="Arial 24",relief=RAISED)
+btn2=Button(aken, text="autoriseerimine", font="Arial 24",relief=RAISED,command=autoriseerimine)
+btn3=Button(aken, text="nime voi parooli muutmine", font="Arial 24",relief=RAISED,command=muuda)
+btn4=Button(aken, text="unustanud parooli taastamine", font="Arial 24",relief=RAISED,command=help)
+btn5=Button(aken, text="lopetamine", font="Arial 24",relief=RAISED,command=end)
+
 
 
 ob=[btn1,btn2,btn3,btn4,btn5]
-for i in range(len( ob)):
+for i in range(len(ob)):
     ob[i].pack()
 aken.mainloop()
